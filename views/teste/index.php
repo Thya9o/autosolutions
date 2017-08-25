a354034ae06878ea970cbd805d2add58b30cf900
Big Boss <thya9o@outlook.com>
2017-08-25 20:01:36 -0300

    Master Update
    Project Starter Pack

---
+++ views/teste/index.php
@@ -0,0 +1,13 @@
+<?php
+$this->title = 'Teste de página';
+?>
+<div class="site-index">
+
+    <div class="jumbotron">
+        <h1>Parabéns!</h1>
+    </div>
+
+    <div class="body-content">
+		<h2>Teste</h2>
+    </div>
+</div>
